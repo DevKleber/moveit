e707bf4ea3d1de52bdde1dad0836d40af7558329
kleber <kleber107@gmail.com>
2021-02-26 22:01:23 -0300

    challenges and context

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,20 +1,51 @@
 import { createContext, ReactNode, useState } from "react";
+import {
+	ChallangesProviderPros,
+	ChallengesContextData,
+} from "./ChallengesContextTypes";
+import challenges from "../../challenges.json";
 
-export const ChallengesContext = createContext({});
-
-interface ChallangesProviderPros {
-	children: ReactNode;
-}
+export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallangesProviderPros) {
-	const [level, setLevel] = useState(1);
+	const [level, setLevel] = useState(11);
+	const [currentExperience, setCurrentExperience] = useState(30);
+	const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+	const [activeChallenge, setActiveChallenge] = useState(null);
+
+	const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
 	function levelUp() {
 		setLevel(level + 1);
 	}
 
+	function startNewChallenge() {
+		const randomChallengeIndex = Math.floor(
+			Math.random() * challenges.length
+		);
+		const challenge = challenges[randomChallengeIndex];
+
+		setActiveChallenge(challenge);
+	}
+
+	function resetChallenge() {
+		setActiveChallenge(null);
+	}
+
 	return (
-		<ChallengesContext.Provider value={{ level, levelUp }}>
+		<ChallengesContext.Provider
+			value={{
+				level,
+				currentExperience,
+				challengesCompleted,
+				activeChallenge,
+				experienceToNextLevel,
+				levelUp,
+				startNewChallenge,
+				resetChallenge,
+			}}
+		>
 			{children}
 		</ChallengesContext.Provider>
 	);
---
+++ src/contexts/ChallengesContextTypes.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import challenges from "../../challenges.json";
+
+export interface ChallangesProviderPros {
+	children: ReactNode;
+}
+
+export interface Challenge {
+	type: "body" | "eye";
+	description: string;
+	amount: number;
+}
+
+export interface ChallengesContextData {
+	//tipo de retorno para facilitar no uso do context.
+	level: number;
+	currentExperience: number;
+	challengesCompleted: number;
+	experienceToNextLevel: number;
+	activeChallenge: Challenge;
+	levelUp: () => void; //é assim que tipa funções sem retorno.
+	startNewChallenge: () => void;
+	resetChallenge: () => void;
+}
